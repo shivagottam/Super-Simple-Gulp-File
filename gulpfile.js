4938aaac488c4cd0ecb0efaa82ff316c04353b11
shivagottam <85722878+shivagottam@users.noreply.github.com>
2022-09-09 07:37:36 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,23 @@ var cssnano = require('gulp-cssnano');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var sass = require('gulp-sass')(require('sass'));
+//const gulp = require('gulp');
+// const sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));//sass-css
+// const sass
+//'use strict';
+ //  const sass = require('gulp-sass')(require('sass'));
 
 // Sass task: compiles the style.scss file into style.css
 gulp.task('sass', function(){
     return gulp.src('app/style.scss')
         .pipe(sass()) // compile SCSS to CSS
         .pipe(cssnano()) // minify CSS
-        .pipe(gulp.dest('dist')); // put final CSS in dist folder
+        .pipe(gulp.dest('dist/css')); // put final CSS in dist folder
 });
 
 //gulp.task('js', function(){
- //   const sass = require('gulp-sass')(require('sass'));
+  //  const pass = require('gulp-sass')(require('sass'));
 //});
 
 // JS task: concatenates and uglifies JS files to script.js
@@ -29,13 +34,13 @@ gulp.task('js', function(){
 // Watch task: watch SCSS and JS files for changes
 gulp.task('watch', function(){
     gulp.watch('app/*.scss', gulp.series('sass'));
-    gulp.watch('app/js/**/*.js', gulp.series('js'));    
+    gulp.watch('app/js/**/*.js', gulp.series('js'));
 });
 
 // Watch task: watch CSS and JS files for changes
 gulp.task('css', function(){
     gulp.watch('app/*.css', gulp.series('css'));
-    gulp.watch('app/js/**/*.js', gulp.series('js')); 
+    gulp.watch('app/js/**/*.js', gulp.series('js'));
 });
 
 // Default task
