7c4d56aa2412ab6452932578bfbf1aa01905b71f
shivagottam <85722878+shivagottam@users.noreply.github.com>
2022-09-09 09:31:30 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 // Initialize modules
 var gulp = require('gulp');
 var cssnano = require('gulp-cssnano');
-//var sass = require('gulp-sass');
+var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 //const gulp = require('gulp');
 // const sass = require('gulp-sass');
-var sass = require('gulp-sass')(require('sass'));//sass-css
+//var sass = require('gulp-sass')(require('sass'));//sass-css
 // const sass
 //'use strict';
  //  const sass = require('gulp-sass')(require('sass'));
